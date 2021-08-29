4ea9508efe53d3eb66680d3ac6951f929477ae14
rito528 <rotoyutoriapp@gmail.com>
2021-08-29 20:09:16 +0900

    手動セーブ機能、コマンドヘルプ機能追加

--- src/main/scala/com/gmail/rotoyutoriapp/AutoSaveBackup.scala
+++ src/main/scala/com/gmail/rotoyutoriapp/AutoSaveBackup.scala
@@ -1,21 +1,43 @@
 package com.gmail.rotoyutoriapp
 
+import org.bukkit.command.{Command, CommandSender}
 import org.bukkit.plugin.java.JavaPlugin
 
 class AutoSaveBackup extends JavaPlugin {
+
+  private val save = new Save(this)
 
   override def onEnable(): Unit = {
     super.onEnable()
     com.gmail.rotoyutoriapp.getConfig.config = getConfig
     saveDefaultConfig()
-    new Save(this).start()
+    save.start()
     getLogger.info("AutoSaveBackup enabled.")
   }
 
   override def onDisable(): Unit = {
     super.onDisable()
-    new Save(this).stop()
+    save.stop()
     getLogger.info("AutoSaveBackup disabled.")
+  }
+
+  @Override
+  override def onCommand(sender: CommandSender, command: Command, label: String, args: Array[String]): Boolean = {
+    super.onCommand(sender, command, label, args)
+    if (label.equalsIgnoreCase("asb")) {
+      args(0) match {
+        case "save" =>
+          save.saveWorld()
+          return true
+        case "help" =>
+          sender.sendMessage("+-----------------------------------+")
+          sender.sendMessage("コマンド一覧")
+          sender.sendMessage("/asb save - ワールドセーブを行います。")
+          sender.sendMessage("+-----------------------------------+")
+          return true
+      }
+    }
+    false
   }
 
 }
